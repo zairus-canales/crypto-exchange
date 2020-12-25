5a6476abd1dcf725a7df05189ad813df3435cd2e
zairus-canales <zairusc03@gmail.com>
2020-12-25 15:54:41 +0700

    modification

--- src/components/Coin/Coin.jsx
+++ src/components/Coin/Coin.jsx
@@ -1,13 +1,21 @@
 import React, { Component } from 'react'
+import './Coin.css';
+import PropTypes from 'prop-types';
 
 export default class Coin extends Component {
     render() {
         return (
-            <tr className="coin">
+            <tr className="coin-row">
                 <td>{this.props.name}</td>
                 <td>{this.props.ticker}</td>
                 <td>{this.props.price}</td>
           </tr>
-        )
+        );
     }
+}
+
+Coin.propTypes = {
+    name: PropTypes.string.isRequired,
+    ticker: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired
 }
