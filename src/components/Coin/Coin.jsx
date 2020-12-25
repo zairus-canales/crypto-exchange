0b14da4d0b27bfe9a3d5da55870f8baa6db45027
zairus-canales <zairusc03@gmail.com>
2020-12-26 03:07:41 +0700

    modification "added refresh button to coin"

--- src/components/Coin/Coin.jsx
+++ src/components/Coin/Coin.jsx
@@ -3,12 +3,34 @@ import './Coin.css';
 import PropTypes from 'prop-types';
 
 export default class Coin extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            price: this.props.price
+        }
+    this.handleClick = this.handleClick.bind(this);
+    }
+    
+    handleClick(event) {
+        event.preventDefault();
+
+        const randomPercentage = 0.995 + Math.random() * 0.01;
+
+            this.setState( function(oldState) {
+                return {
+                    price: oldState.price * randomPercentage
+                };
+            });        
+   }
     render() {
         return (
             <tr className="coin-row">
                 <td>{this.props.name}</td>
                 <td>{this.props.ticker}</td>
-                <td>{this.props.price}</td>
+                <td>${this.state.price}</td>
+                <td>
+                    <button onClick={this.handleClick}>Refresh</button>
+                </td>
           </tr>
         );
     }
