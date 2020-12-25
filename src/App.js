c6a2a3107d3abc102016cf8296696796fde6a1c7
zairus-canales <zairusc03@gmail.com>
2020-12-25 15:52:34 +0700

    modification

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Coin from './components/Coin/Coin';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img src={logo} alt="React logo" className="App-logo"/>
         <h1>
           Crypto Exchange
         </h1>
       </header>
-      <table>
+      <table >
         <thead>
           <tr>
             <th>Name</th>
@@ -19,8 +21,8 @@ function App() {
           </tr>
         </thead>
         <tbody>
-          <Coin name="Bitcoin" ticker="BTC" price="20000.00"/>
-          <Coin name="Ethereum" ticker="ETH" price="600.00"/>
+          <Coin name="Bitcoin" ticker="BTC" price={20000.00}/>
+          <Coin name="Ethereum" ticker="ETH" price={600.00}/>
         </tbody>
       </table>
     </div>
